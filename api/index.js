1f358ab032132b3838d23e5e1c8f2cc86b907baa
Odii Daniel <xenithheight@gmail.com>
2025-02-09 21:29:25 +0100

    RRR

--- api/index.js
+++ api/index.js
@@ -38,20 +38,6 @@ app.use(cors({
     allowedHeaders: 'Content-Type, Authorization'
 }));
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://fynance-alpha.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-    // Handle preflight requests
-    if (req.method === "OPTIONS") {
-        return res.status(200).end();
-    }
-
-    next();
-});
-
-
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
